6eac76f04a0e09dae2b4d694e2e1c80a72d37bc1
anuramwork <anuramanilwork@gmail.com>
2025-05-30 12:41:33 +0530

    added doctors page and added filters for specialities

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { specialityData } from "../assets/assets";
 const Doctors = () => {
+  const { speciality } = useParams();
+  const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
+  const [filterDoc, setFilterDoc] = useState([]);
+
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter((doc) => doc.speciality === speciality));
+    } else {
+      setFilterDoc(doctors);
+    }
+  };
+
+  // add speciaity on dependancy array
+  useEffect(() => {
+    applyFilter();
+  }, [doctors, speciality]);
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <p className="text-gray-600">Browse through the doctors specialist.</p>
+      <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <div className="flex flex-col gap-4 text-sm text-gray-600">
+          {specialityData.map((item, index) => {
+            return (
+              <p
+                onClick={() => {
+                  speciality === item.speciality
+                    ? navigate("/doctors")
+                    : navigate(`/doctors/${item.speciality}`);
+                }}
+                key={index}
+                className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${
+                  speciality === item.speciality
+                    ? "bg-indigo-100 text-black"
+                    : ""
+                }`}
+              >
+                {item.speciality}
+              </p>
+            );
+          })}
 
-export default Doctors
+          {/* <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p> */}
+        </div>
+
+        <div className="w-full grid grid-cols-auto gap-4 gap-y-6 ">
+          {filterDoc.map((item, index) => (
+            <div
+              onClick={() => {
+                navigate(`/appointment/${item._id}`);
+              }}
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+              key={index}
+            >
+              <img className="bg-blue-50" src={item.image} alt="" />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                  <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                  <p>Available</p>
+                </div>
+                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+                <p className="text-gray-600 text-sm">{item.speciality}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Doctors;
