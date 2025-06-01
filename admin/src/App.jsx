da8b139f8fd5339ceef9f1a05b1fd37b370cc6b5
anuramwork <anuramanilwork@gmail.com>
2025-06-02 00:27:57 +0530

    created sidebar and navbar components and made all pages in admin folder(yet to code)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import { useContext } from 'react';
 import { AdminContext } from './context/adminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctors from './pages/Admin/AddDoctors';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
 
@@ -14,6 +20,16 @@ const App = () => {
       
       <ToastContainer/>
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route path="/" element={<></>}/>
+          <Route path="/admin-dashboard" element={<Dashboard/>}/>
+          <Route path="/all-appointments" element={<AllAppointments/>}/>
+          <Route path="/add-doctor" element={<AddDoctors/>}/>
+          <Route path="/doctor-list" element={<DoctorsList/>}/>
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
