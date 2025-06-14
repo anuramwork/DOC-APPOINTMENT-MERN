988b08724a70b267d5ef77deb3f1920a724af000
anuramwork <anuramanilwork@gmail.com>
2025-06-14 18:52:08 +0530

    changed availability color, grey not working(don't know why) so set it to red

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -82,13 +82,11 @@ const Doctors = () => {
               <img className="bg-blue-50" src={item.image} alt="" />
               <div className="p-4">
                 <div
-                  className={`flex items-center gap-2 text-sm text-center ${
-                    item.available ? "text-green-500" : "text-gray-500"
-                  } `}
+                  className={`flex items-center gap-2 text-sm text-center`}
                 >
                   <p
                     className={`w-2 h-2 ${
-                      item.available ? "bg-green-500" : "bg-gray-500"
+                      item.available ? "bg-green-500" : "bg-red-500"
                     } bg-green-500 rounded-full`}
                   ></p>
                   <p>{item.available ? "Available" : "Not Available"}</p>
