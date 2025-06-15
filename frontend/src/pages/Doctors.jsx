ec2f13509457765ed3d00d761cfc6f6482281841
anuramwork <anuramanilwork@gmail.com>
2025-06-15 23:42:40 +0530

    corrected availability colors on Doctors page

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -92,12 +92,14 @@ const Doctors = () => {
                 <img className="bg-blue-50" src={item.image} alt="" />
                 <div className="p-4">
                   <div
-                    className={`flex items-center gap-2 text-sm text-center`}
+                    className={`flex items-center gap-2 text-sm text-center ${
+                        item.available ? "text-green-500" : "text-gray-500"
+                      }`}
                   >
                     <p
                       className={`w-2 h-2 ${
-                        item.available ? "bg-green-500" : "bg-red-500"
-                      } bg-green-500 rounded-full`}
+                        item.available ? "bg-green-500" : "bg-gray-500"
+                      } rounded-full`}
                     ></p>
                     <p>{item.available ? "Available" : "Not Available"}</p>
                   </div>
